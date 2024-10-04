6a1e46174cb6d7500bf23aa88563a4567d7d904f
albin122 <albinphilipgigi@gmail.com>
2024-10-04 16:08:29 +0530

    Add files via upload

---
+++ pythonlab.py
@@ -0,0 +1,36 @@
+
+
+"""print("Hello World")
+if 9 > 6:
+    print("nine is greater than six")
+    if 5 > 2:
+        print("five is greater than two")
+        if 4 > 3:
+            print("four is greater than three")
+
+            x = str(2)
+            y = int(3)
+            z = float(3)
+            print(x,y,z)
+            x , y , z = ("orange","apple","cherry")
+            print(x)
+            print(y)
+            print(z)
+            x = "a"
+            y = "new"
+            z = "car"
+            print(x+y+z)"""
+"""x = int(80)
+y = int(70)
+z = int(60)
+sum = x+y+z
+avg =sum/3
+print(avg)
+username = input("enter username")
+print("the username is " +username)"""
+
+
+
+
+
+
