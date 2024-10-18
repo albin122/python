de8e5932b6f4d59218034535e919ebf9ddbbd1b4
albin122 <albinphilipgigi@gmail.com>
2024-10-18 14:32:05 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,10 @@
+'''
+Author: ALBIN PHILIP GIGI
+Date : 18/10/2024
+Description :a Python program that asks the user to input their name and age.
+After receiving the input, display a message that includes the user's name and age.
+
+'''
+name=input("enter your name;")
+age=input("enter your age;")
+print("HELLO,",name,"! you are",age,"years old")
