8636195595c37a2a4ccd2656bf1812a0968d4704
albin122 <albinphilipgigi@gmail.com>
2024-10-18 16:20:51 +0530

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,9 @@
+'''
+Auther: albin philip gigi
+date : 18/10/2006
+Description :a Python program that uses functions from the math module to perform the following operations on a number provided by the user
+'''
+import math
+number=int(input("enter a number"))
+square_root=math.sqrt(number)
+print("Square_root of ",number,':',square_root)
