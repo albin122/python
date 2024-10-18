6d8e9d5a18c151958d9099fdc427eed698294710
albin122 <albinphilipgigi@gmail.com>
2024-10-18 15:07:41 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,12 @@
+'''
+Auther: albin philip gigi
+date : 18/10/2006
+Description :a Python program that stores a string in a variable.
+ Extract a specific part of the string (substring) and then concatenate it with another string.
+  Finally, display the new string.
+'''
+str1="Hello, World!"
+substring=str1[7:12]
+str2="Everyone!"
+substring1=substring+str2
+print(substring1)
