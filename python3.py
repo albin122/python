d18c540dba5e12b6cc1a851e389b713b1beb3192
albin122 <albinphilipgigi@gmail.com>
2024-10-18 16:00:06 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,13 @@
+'''
+Auther: albin philip gigi
+date : 18/10/2006
+Description :a Python program that demonstrates the usage of arithmetic, comparison, and logical operators.
+Perform a few operations and print the results.
+'''
+number1=int(input("enter your first number"))
+number2=int(input("enter your second number"))
+print("sum=",number1+number2,",division=",number1/number2)
+print("Is number1 greater than number2?;",number1>number2)
+print("Are number1 and number2 equal?;",number1==number2)
+print("Logical AND;",number1>0 and number2>0)
+print("Logical OR:",number1<0 or number2<0)
