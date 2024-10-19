d8c059402cbde05c8497db25b541459d53007b36
albin122 <albinphilipgigi@gmail.com>
2024-10-19 14:51:00 +0530

    Add files via upload

---
+++ pythonlab6.py
@@ -0,0 +1,20 @@
+'''
+Author :Albin Philip Gigi
+Date : 19/10/2024
+Descrpition :a Python program that performs the following tasks
+'''
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+add=num1+num2
+print("sum of num1 and num2 is:",add)
+subt=num2-num1
+print("The difference when num2 is subtracted from num1 is:",subt)
+multi=num1*num2
+print("The product of num1 and num2 is: result:",multi)
+div=num1/num2
+print("The quotient when num1 is divided by num2 is: result:",div)
+mod=num1%num2
+print("The remainder when num1 is divided by num2 is:",mod)
+god=(num1+num2)*num3/2
+print("The result of (num1 + num2) * num3 / 2 is:",god)
