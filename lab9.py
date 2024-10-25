244f21bf9204efd479c50df6b84b6fb9af083d33
albin122 <albinphilipgigi@gmail.com>
2024-10-25 14:25:21 +0530

    Add files via upload

---
+++ lab9.py
@@ -0,0 +1,6 @@
+number1=int(input("enter your first number"))
+number2=int(input("enter your second number"))
+if number1>number2:
+    print(number1,"is greater than",number2)
+else:
+    print(number2,"is greater than ",number1)
