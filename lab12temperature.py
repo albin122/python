f57f1188656b6d659b5cf304dd888859f8e6f9b9
albin122 <albinphilipgigi@gmail.com>
2024-10-25 16:02:37 +0530

    Add files via upload

--- lab12temperature.py
+++ lab12temperature.py
@@ -4,13 +4,13 @@ Date : 25/10/2024
 Descrpition :a Python program to convert temperature values back and forth between Celsius and Fahrenheit.
  The user should be able to input a temperature in Celsius or Fahrenheit,
  and the program should convert it to the other unit using the formula:
-c5=f−329
+c/5=f−32/9
 '''
 temperature=int(input("enter temperature"))
 scale= input("Is this in (C)elsius or (F)ahrenheit?")
 if scale=="C":
     f=(9/5*temperature)+32
-    print(temperature,"celsiusis is",f,"fahrenheit.")
+    print(temperature,"celsiuss is",f,"fahrenheit.")
 else:
     c=5/9*(temperature-32)
     print(temperature,"fahrenheit is,",c,"celsiusis.")
