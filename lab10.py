a93504e8da31f375becdfac56e3b7c44c943cb55
albin122 <albinphilipgigi@gmail.com>
2024-10-25 14:38:39 +0530

    Add files via upload

---
+++ lab10.py
@@ -0,0 +1,7 @@
+number1=int(input("enter your first number"))
+if number1>0:
+    print(number1," is a positive number")
+elif number1<0:
+    print(number1,"is a negative number")
+else:
+    print(number1, "is a Zero")
