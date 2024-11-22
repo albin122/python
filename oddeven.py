4abe353dc93d699c720f30c64da3107ba6ddc310
albin122 <albinphilipgigi@gmail.com>
2024-11-22 14:13:50 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,14 @@
+list1=[20,43,33,65,74,35]
+list2=[82,24,51,32,79,12]
+combined_list=list1+list2
+even_list=[]
+odd_list=[]
+for i in combined_list:
+    if i%2==0:
+        even_list.append(i)
+    else:
+        odd_list.append(i)
+even_list.sort()
+odd_list.sort()
+mergedlist=even_list+odd_list
+print(mergedlist)
