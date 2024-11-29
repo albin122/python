bde240910c7f0ff3110f602f7d59344b80329982
albin122 <albinphilipgigi@gmail.com>
2024-11-29 15:08:00 +0530

    Add files via upload

--- phnnumbercheck.py
+++ phnnumbercheck.py
@@ -1,3 +1,12 @@
+'''
+Author: ALBIN PHILIP GIGI
+Date : 29/11/2024
+Description:Program to check whether the given number is a valid mobile number or not using functions.
+
+Rules:
+    Every number should contain exactly 10 digits.
+    The first digit should be 7 or 8 or 9
+'''
 def phn_number():
     num=input("enter phone number")
     if len(num)==10 and num[0] in ["9","8","7"]:
