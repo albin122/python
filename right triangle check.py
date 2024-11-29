173776945a74a83a801f879ae4af2ab73df34be0
albin122 <albinphilipgigi@gmail.com>
2024-11-29 15:08:15 +0530

    Add files via upload

--- right triangle check.py
+++ right triangle check.py
@@ -1,3 +1,10 @@
+'''
+Author: ALBIN PHILIP GIGI
+Date : 29/11/2024
+Description:A program that accepts the lengths of three sides of a triangle as inputs.
+The program should output whether or not the triangle is a right triangle (Recall from the Pythagorean Theorem that in a right triangle,
+  the square of one side equals the sum of the squares of the other two sides). Implement using functions.
+'''
 def triangle():
     l1=[]
     side1=int(input("enter first side of triangle"))
