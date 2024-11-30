9fae00c2cf414fa5d072588e0a9e3108dc4956df
albin122 <albinphilipgigi@gmail.com>
2024-11-30 16:25:59 +0530

    Add files via upload

---
+++ max three no.py
@@ -0,0 +1,10 @@
+def maximum():
+    s1=[]
+    n1=int(input('enter the number'))
+    s1.append(n1)
+    n2=int(input('enter the number'))
+    s1.append(n2)
+    n3=int(input('enter the number'))
+    s1.append(n3)
+    print(max(s1))
+maximum()
