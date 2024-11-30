ca9395b427f5df0c33eaf7e77f6a39126a5078d7
albin122 <albinphilipgigi@gmail.com>
2024-11-30 16:26:21 +0530

    Add files via upload

---
+++ reverse list.py
@@ -0,0 +1,9 @@
+
+def reverse(str1):
+    str1[::-1]
+str='56789'
+print("orginal string",str)
+print("reverse string",reverse('56789'))
+str='reverse'
+print("\nOriginal string:", str)
+print("Reverse string:", reverse(str))
